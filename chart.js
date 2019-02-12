7bf6a3f0d6d1975770eca68ca779280d83cbbdad
David J Mendez <31456061+Djmendez5@users.noreply.github.com>
2019-02-12 15:14:49 -0800

    Add files via upload

--- chart.js
+++ chart.js
@@ -109,13 +109,15 @@ let letterScale
   //for(var l=2; l<data2.length;l++){
    letterScale = d3.scaleBand()
      //.domain([data2[l]["Incident Category"], data2[l-1]["Incident Category"],  data2[l-2]["Incident Category"],data2[l]["Incident Category"]]) // all letters (not using the count here)
-     .domain(["Other Offenses", "Motor Vehicle Theft?", "Rape","Drug Violantion","Suicide","Homocide","Liquor Laws", "Suspecious","Vehicle Impounded","Juvenile Offenses","Vehicle Misplaced", "Family Offense", "Fire Report", "Embezzlement", "Vandalism","Traffic Collision","Prostitution" ,"Civil Sidewalks","Courtesy Report", "Arson","Case Closure","Sex Offense","Weapons Carrying Etc","Forgery And Counterfeiting", "Stolen Property", "Weapons Offense", "Other", "Miscellaneous Investigation","Traffic Violation Arrest", "Disorderly Conduct", "Suspicious Occ","Drug Offense", "Offences Against The Family And Children", "Missing Person", "Fraud", "Recovered Vehicle", "Robbery", "Motor Vehicle Theft", "Lost Property","Warrant", "Burglary", "Malicious Mischief","Non-Criminal", "Assault","Other Miscellaneous", "Larceny Theft","Gambling", "Motor Vehicle Theft?", "Rape","Drug Violation", "Suicide","Homocide", "Liquor Laws", "Suspicious", "Vehicle Impounded"])
+     .domain(["Assault", "Burglary", "Arson","Case Closure","Civil Sidewalks","Courtesy Report", "Disorderly Conduct","Drug Offense","Drug Violantion","Embezzlement", "Family Offense", "Fire Report","Forgery And Counterfeiting", "Fraud","Gambling","Homocide","Juvenile Offenses", "Larceny Theft","Liquor Laws", "Lost Property","Malicious Mischief","Miscellaneous Investigation","Missing Person","Motor Vehicle Theft?","Motor Vehicle Theft","Non-Criminal", "Offences Against The Family And Children", "Other","Other Miscellaneous","Other Offenses","Prostitution", "Rape", "Recovered Vehicle", "Robbery","Sex Offense", "Stolen Property","Suicide", "Suspecious", "Suspicious Occ","Traffic Collision", "Traffic Violation Arrest", "Vandalism","Vehicle Impounded","Vehicle Misplaced","Warrant","Weapons Carrying Etc", "Weapons Offense", "Motor Vehicle Theft?", "Rape","Drug Violation", "Suicide","Homocide", "Suspicious", "Vehicle Impounded"])
     .rangeRound([0, plotWidth])
     .paddingInner(0.1); // space between bars
 //console.log(data2[l]["Incident Category"])
 //}
   // try using these scales in the console
   // console.log("count scale [0, 36]:", [countScale(0), countScale(36)]);
+
+
   // console.log("letter scale [a, z]:", [letterScale('a'), letterScale('z')]);
 
   // we are actually going to draw on the "plot area"
@@ -198,7 +200,7 @@ let letterScale
       return "red";
     } else if(d.key==="Non-Criminal"){
       return "blue"
-    }else if(d.key==="Case Closure" || d.key==="Civil Sidewalks" || d.key==="Courtesy Report" || d.key==="Emblezzement" || d.key==="Fraud" || d.key==="Liquor Laws" || d.key==="Lost Property" || d.key==="Miscellaneous Investigation" || d.key==="Missing Person" || d.key==="Other" || d.key==="Other Miscellaneous" || d.key==="Other Offenses" || d.key==="Recovered Vehicle" || d.key==="Suicide" || d.key==="Traffic Collision" || d.key==="Traffic Violation Arrest" || d.key==="Vehicle Impounded") {  //green
+    }else if(d.key==="Case Closure" || d.key==="Civil Sidewalks" || d.key==="Courtesy Report" || d.key==="Embezzlement" || d.key==="Fraud" || d.key==="Liquor Laws" || d.key==="Lost Property" || d.key==="Miscellaneous Investigation" || d.key==="Missing Person" || d.key==="Other" || d.key==="Other Miscellaneous" || d.key==="Other Offenses" || d.key==="Recovered Vehicle" || d.key==="Suicide" || d.key==="Traffic Collision" || d.key==="Traffic Violation Arrest" || d.key==="Vehicle Impounded") {  //green
       return "green";
     } else if (d.key === "Drug Offenses" || d.key==="Disordely Conduct" || d.key==="Drug Violation" || "Fire Report" || "Forgery And Counterfeiting" || d.key==="Larceny Theft" || d.key==="Malicious Mischief" || d.key==="Motor Vehicle Theft" || d.key==="Motor Vehicle Theft?" || d.key==="Offenses Against The Family And Children" || d.key==="Prostitution" || d.key==="Stolen Property" || d.key==="Suspecious" || d.key==="Suspecious Occ" || d.key==="Vandalism"|| d.key==="Weapons Carrying Etc") { //Orange
       return "orange";
